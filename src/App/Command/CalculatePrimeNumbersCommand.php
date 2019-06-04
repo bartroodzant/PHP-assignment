3d39d8e0734bce066379a4982510cf71bb4cac85
Bart Roodzant <bartroodzant@hotmail.com>
2019-06-04 15:57:22 +0200

    Removed CalculatePrimeNumbersService and rewrote the calculatePrimeNumbers algorithm to use the Sieve of Eratosthenes.

--- src/App/Command/CalculatePrimeNumbersCommand.php
+++ src/App/Command/CalculatePrimeNumbersCommand.php
@@ -4,7 +4,6 @@ namespace App\Command;
 
 
 use App\Model\PrimeNumber;
-use App\Services\CalculatePrimeNumbersService;
 use DOMDocument;
 use DOMNode;
 use Symfony\Component\Console\Command\Command;
@@ -16,6 +15,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 class CalculatePrimeNumbersCommand extends Command {
 
     const START_INTEGER = 0;
+    /**
+     * @var ProgressBar
+     */
+    private $progressBar;
 
     protected function configure() {
         $this->setName('calculate')
@@ -31,9 +34,7 @@ class CalculatePrimeNumbersCommand extends Command {
      * @return int|void|null
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $calculatePrimeNumbersService = new CalculatePrimeNumbersService();
         $range = $input->getArgument('range');
-        $primeNumberArray = [];
 
         $domDocument = new DOMDocument('1.0', 'UTF-8');
         $xmlRoot = $domDocument->createElement("xml");
@@ -41,20 +42,14 @@ class CalculatePrimeNumbersCommand extends Command {
 
         $output->writeln('<info>Starting calculations</info>');
 
-        $progressBar = new ProgressBar($output, $range);
-        $progressBar->setBarCharacter('<fg=green>=</>');
-        $progressBar->setProgressCharacter("\xF0\x9F\x8D\xBA");
+        $limit = intval(sqrt($range));
 
-        for($i = self::START_INTEGER; $i < $range; $i += 1) {
-            if($calculatePrimeNumbersService->isPrimeNumber($i)) {
-                $primeNumber = $this->createPrimeNumberObject($primeNumberArray, $i);
+        $this->progressBar = new ProgressBar($output, $limit);
+        $this->progressBar->setBarCharacter('<fg=green>=</>');
+        $this->progressBar->setProgressCharacter("\xF0\x9F\x8D\xBA");
 
-                $this->addPrimeNumberToXml($domDocument, $xmlRoot, $primeNumber);
-
-                $primeNumberArray[] = $primeNumber;
-            }
-            $progressBar->advance();
-        }
+        $valueArray = $this->calculatePrimeNumbers($limit, $range);
+        $primeNumberArray = $this->createPrimeNumberObjectsAndXMLObjectsFromArray($domDocument, $xmlRoot, $valueArray);
 
         $output->writeln('');
 
@@ -64,6 +59,55 @@ class CalculatePrimeNumbersCommand extends Command {
 
         $outputString = $this->primeNumberArrayToRomanNumeralString($primeNumberArray);
         $output->writeln($outputString);
+    }
+
+    /**
+     * @param int $limit
+     * @param $range
+     * @return array
+     */
+    private function calculatePrimeNumbers(int $limit, int $range): array
+    {
+        $valueArray = array_fill(0, $range, true);
+
+        for ($i = 2; $i <= $limit; $i += 1) {
+            if ($valueArray[$i - 1]) {
+                for ($j = $i * $i; $j <= $range; $j += $i) {
+                    $valueArray[$j - 1] = false;
+                }
+            }
+            $this->progressBar->advance();
+        }
+        $valueArray[0] = false;
+        return $valueArray;
+    }
+
+    /**
+     * @param DOMDocument $domDocument
+     * @param DOMNode $xmlRoot
+     * @param array $valueArray
+     * @return array
+     */
+    private function createPrimeNumberObjectsAndXMLObjectsFromArray(DOMDocument $domDocument, DOMNode $xmlRoot, array $valueArray): array
+    {
+        $primeNumberArray = [];
+        $count = 0;
+        foreach ($valueArray as $i => $is_prime) {
+            if ($is_prime) {
+                $count += 1;
+                $value = $i + 1;
+
+                $primeNumber = new PrimeNumber();
+                $primeNumber->setValue($value)
+                    ->setCountFromZero($count)
+                    ->calculateAndSetRomanLiteral($value);
+
+                $this->addPrimeNumberToXml($domDocument, $xmlRoot, $primeNumber);
+
+                $primeNumberArray[] = $primeNumber;
+            }
+        }
+        return $primeNumberArray;
     }
 
     /**
@@ -84,20 +128,6 @@ class CalculatePrimeNumbersCommand extends Command {
     }
 
     /**
-     * @param array $primeNumberArray
-     * @param int $value
-     * @return PrimeNumber
-     */
-    private function createPrimeNumberObject(array $primeNumberArray, int $value): PrimeNumber {
-        $primeNumber = new PrimeNumber();
-        $countFromZero = sizeof($primeNumberArray) + 1;
-        $primeNumber->setValue($value)
-            ->setCountFromZero($countFromZero)
-            ->calculateAndsetRomanLiteral($value);
-        return $primeNumber;
-    }
-
-    /**
      * @param DOMDocument $domDocument
      * @param DOMNode $xmlRoot
      * @param PrimeNumber $primeNumber
@@ -110,4 +140,5 @@ class CalculatePrimeNumbersCommand extends Command {
         $currentTrack->appendChild($domDocument->createElement('countFromZero', $primeNumber->getCountFromZero()));
         $currentTrack->appendChild($domDocument->createElement('romanLiteral', $primeNumber->getRomanLiteral()));
     }
+
 }
